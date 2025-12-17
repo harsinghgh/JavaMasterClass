3926cd12d38558c20b0d5d190685fbd8bf1a221f
Harpreet Singh <harpreet.bindra@outlook.com>
2025-12-17 17:51:12 -0500

    adding conditional logic

---
+++ JavaMasterCLass/src/condition.java
@@ -0,0 +1,16 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class condition {
+    static void main(String[] args) {
+        System.out.println("Welcome Harpreet");
+
+        boolean isAlien = false;
+        if(isAlien == true){
+            System.out.println("You are an Alien");
+        }
+        else {
+            System.out.println("You are not an Alien");
+        }
+
+    }
+}
