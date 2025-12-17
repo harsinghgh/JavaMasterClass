d1fd331f3e52811ee0381cb0137a84b160f140eb
Harpreet Singh <harpreet.bindra@outlook.com>
2025-12-17 17:51:12 -0500

    adding conditional logic

---
+++ JavaMasterCLass/src/condition.java
@@ -0,0 +1,15 @@
+
+public class condition {
+    static void main(String[] args) {
+        System.out.println("Welcome Harpreet");
+
+        boolean isAlien = false;
+        if(isAlien == true){
+            System.out.println("You are an Alien");
+        }
+        else {
+            System.out.println("You are not an Alien");
+        }
+
+    }
+}
